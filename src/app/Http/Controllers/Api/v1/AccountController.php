32cd69644e5c63e52d0f63ebd9d5d50061a45417
AllhailLesha <a.goponenko0098@gmail.com>
2024-10-23 21:11:00 +0300

    Добавил получение данных по аккаунту

--- src/app/Http/Controllers/Api/v1/AccountController.php
+++ src/app/Http/Controllers/Api/v1/AccountController.php
@@ -6,6 +6,8 @@ use App\Facades\Account;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Account\CreateAccountRequest;
 use App\Http\Requests\Account\SignInAccountRequest;
+use App\Http\Resources\Account\UserResource;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 
 class AccountController extends Controller
@@ -20,5 +22,12 @@ class AccountController extends Controller
         return [
             "token" => $request->signIn()
         ];
+    }
+
+    public function show(): UserResource
+    {
+        return new UserResource(
+            auth()->user()
+        );
     }
 }
